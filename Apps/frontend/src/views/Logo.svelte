1b0cf0ecdf913e4531b09fe157adee75796a1ce3
Leander Tolksdorf <leandet98@zedat.fu-berlin.de>
2022-11-06 11:34:41 +0100

    #23: Add Logo to page

---
+++ Apps/frontend/src/views/Logo.svelte
@@ -0,0 +1,22 @@
+<script>
+  import logo from "../assets/sosci-logo.svg";
+</script>
+
+<div>
+  <img src={logo} alt="SOSCI" />
+</div>
+
+<style>
+  div {
+    width: 100%;
+  }
+  img {
+    width: 15rem;
+    filter: invert(100%);
+  }
+  @media (prefers-color-scheme: light) {
+    img {
+      filter: none;
+    }
+  }
+</style>
