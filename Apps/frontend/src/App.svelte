2bea8e7b648bd2fe71269d2ef640c601efe11790
nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>
2022-11-06 12:28:07 +0100

    add svelte component for Btn and add click dispatcher (#26)
    Signed-off-by: nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>

--- Apps/frontend/src/App.svelte
+++ Apps/frontend/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
   import HelloWorld from './views/HelloWorld.svelte'
+  import OnOffButton from './views/OnOffButton.svelte'
 </script>
 
 <main>
   <h1>Hello World</h1>
   <HelloWorld />
+  <OnOffButton on:switch-plot-enabled={async (event)  => {console.log('Turned ' + (event.detail.enabled ? 'on' : 'off') +'...')}} />
 </main>
 
 <style>
