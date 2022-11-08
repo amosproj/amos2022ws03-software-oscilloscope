25929ef38773ad5ffe8bfa2df391be3524044e66
Jan Degen <jan.degen@die-degens.eu>
2022-11-08 19:41:42 +0100

    Removed unused code

--- Apps/frontend/server/main.js
+++ Apps/frontend/server/main.js
@@ -26,16 +26,3 @@ server.on('listening', () => {
 });
 
 server.bind(process.env.HOST_PORT, process.env.HOST_ADDRESS);
-
-function toArrayBuffer(buf) {
-    const ab = new ArrayBuffer(buf.length);
-    const view = new Uint8Array(ab);
-    for (let i = 0; i < buf.length; ++i) {
-        view[i] = buf[i];
-    }
-    return ab;
-}
-
-function ab2str(buf) {
-    return String.fromCharCode.apply(null, new Uint16Array(buf));
-  }
