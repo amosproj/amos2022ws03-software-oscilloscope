6dd963b2659f2e6e66bca9e4eb8b7402160db10e
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-11-18 10:42:01 +0100

    #53: make line colors constant

--- Apps/frontend/src/const.js
+++ Apps/frontend/src/const.js
@@ -1,6 +1,21 @@
+import { ColorRGBA } from "webgl-plot";
+
 export const NR_SHOWN_DATAPOINTS = 1000;
 
 export const CANVAS_WIDTH = 1000;
 export const CANVAS_HEIGHT = 500;
 export const NUM_INTERVALS_X = 20;
 export const NUM_INTERVALS_Y = 10;
+
+export const LINE_COLORS = [
+  [230, 0, 73],
+  [11, 180, 255],
+  [80, 233, 145],
+  [230, 216, 0],
+  [155, 25, 245],
+  [255, 163, 0],
+  [220, 10, 180],
+  [179, 212, 255],
+  [0, 191, 160],
+  [128, 143, 128],
+];
--- Apps/frontend/src/views/SineWave.svelte
+++ Apps/frontend/src/views/SineWave.svelte
@@ -1,59 +1,66 @@
 <script>
   import { beforeUpdate, onMount } from "svelte";
   import { ColorRGBA, WebglLine, WebglPlot } from "webgl-plot";
-  import { CANVAS_HEIGHT, CANVAS_WIDTH, NUM_INTERVALS_Y } from "../const";
+  import {
+    CANVAS_HEIGHT,
+    CANVAS_WIDTH,
+    LINE_COLORS,
+    NUM_INTERVALS_Y,
+  } from "../const";
 
   export let scaleY;
 
   let canvasElement;
   let webGLPlot;
   let numberOfChannels = 10;
-  let channel_samples = Array.from(Array(numberOfChannels), () => new Array(CANVAS_WIDTH).fill(0.0))
-  let lines = []
+  let channel_samples = Array.from(Array(numberOfChannels), () =>
+    new Array(CANVAS_WIDTH).fill(0.0)
+  );
+  let lines = [];
 
-  let x = 0
+  let x = 0;
   export const updateBuffer = (samples) => {
-    for (let channelIndex = 0; channelIndex < channel_samples.length; channelIndex++)
-    {
-      channel_samples[channelIndex][x] = samples[channelIndex]
+    for (
+      let channelIndex = 0;
+      channelIndex < channel_samples.length;
+      channelIndex++
+    ) {
+      channel_samples[channelIndex][x] = samples[channelIndex];
     }
     x++;
     x = x % CANVAS_WIDTH;
-  }
+  };
 
   const update = () => {
-    for (let i = 0; i < channel_samples.length; i++)
-    {
+    for (let i = 0; i < channel_samples.length; i++) {
       for (let x = 0; x < CANVAS_WIDTH; x++) {
         lines[i].setY(x, channel_samples[i][x]);
       }
     }
-  }
+  };
 
   const resizeCanvas = () => {
     canvasElement.width = CANVAS_WIDTH;
     canvasElement.height = CANVAS_HEIGHT;
-  }
+  };
 
   const initializePlot = () => {
     webGLPlot = new WebglPlot(canvasElement);
     initializeLines();
-    console.log(`lines: ${lines.length}`)
-    console.log(`channels: ${channel_samples.length}`)
+    console.log(`lines: ${lines.length}`);
+    console.log(`channels: ${channel_samples.length}`);
+  };
 
-  }
-  
   const initializeLines = () => {
-    for (let i = 0; i < numberOfChannels; i++)
-    {
-      const color = new ColorRGBA(Math.random(), Math.random(), Math.random(), 1);
+    for (let i = 0; i < numberOfChannels; i++) {
+      const color = LINE_COLORS[i];
       let line = new WebglLine(color, CANVAS_WIDTH);
       line.arrangeX();
       line.scaleY = 1 / ((NUM_INTERVALS_Y / 2) * scaleY);
       webGLPlot.addLine(line);
-      lines.push(line)
+      lines.push(line);
     }
-  }
+  };
 
   onMount(() => {
     resizeCanvas();
@@ -68,7 +75,7 @@
     update();
     webGLPlot.update();
     window.requestAnimationFrame(newFrame);
-  }
+  };
 </script>
 
 <canvas bind:this={canvasElement} />
