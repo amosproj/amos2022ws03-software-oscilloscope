55b6cf3934012ebb8ba663520bbc568822330c68
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-11-19 13:31:16 +0100

    #53: add min max lines and display voltage

--- Apps/frontend/src/views/Indicators.svelte
+++ Apps/frontend/src/views/Indicators.svelte
@@ -5,7 +5,8 @@
     CANVAS_WIDTH,
     INDICATOR_DECIMAL_PLACES,
     INDICATOR_FONT_SIZE,
-    INDICATOR_HEIGHT,
+    INDICATOR_MARGIN,
+    INDICATOR_SECTION_WIDTH,
     INDICATOR_WIDTH,
     LINE_COLORS,
     NUM_INTERVALS_Y,
@@ -13,7 +14,8 @@
 
   let canvasElement;
   let canvasContext;
-  let indicators = Array(10).fill({ min: 0, max: 0, current: 0 });
+  let min = Array(10).fill(0.0);
+  let max = Array(10).fill(0.0);
 
   export let scaleY;
   export function update(samples) {
@@ -24,25 +26,27 @@
       canvasElement.height
     );
     for (let i = 0; i < samples.length; i++) {
-      let current = samples[i];
-      indicators[i].current = current;
-      if (current < indicators[i].min) {
-        indicators[i].min = current;
-        return;
+      if (samples[i] < min[i]) {
+        min[i] = samples[i];
       }
-      if (current > indicators[i].max) {
-        indicators[i].max = current;
+      if (samples[i] > max[i]) {
+        max[i] = samples[i];
       }
       drawZeroLine();
       drawIndicator(
-        current,
+        i,
+        samples[i],
+        `rgba(${LINE_COLORS[i][0]}, ${LINE_COLORS[i][1]}, ${LINE_COLORS[i][2]}, 1)`
+      );
+      drawMinMaxLine(
+        i,
         `rgba(${LINE_COLORS[i][0]}, ${LINE_COLORS[i][1]}, ${LINE_COLORS[i][2]}, 1)`
       );
     }
   }
 
   const initializeCanvas = () => {
-    canvasElement.width = CANVAS_WIDTH / 8;
+    canvasElement.width = INDICATOR_SECTION_WIDTH;
     canvasElement.height = CANVAS_HEIGHT;
     canvasContext = canvasElement.getContext("2d");
     canvasContext.translate(canvasElement.width, canvasElement.height / 2);
@@ -50,9 +54,8 @@
 
   const drawZeroLine = () => {
     canvasContext.beginPath();
-    canvasContext.strokeStyle = "#ffffff80";
-    canvasContext.fillStyle = "#ffffff80";
-    canvasContext.setLineDash([5, 5]);
+    canvasContext.strokeStyle = "#ffffff20";
+    canvasContext.fillStyle = "#ffffff20";
     canvasContext.moveTo(0, 0);
     canvasContext.lineTo(-canvasElement.width, 0);
     canvasContext.stroke();
@@ -62,27 +65,40 @@
     canvasContext.fillText("0", -canvasElement.width, INDICATOR_FONT_SIZE);
   };
 
-  const drawIndicator = (voltage, color) => {
+  const drawIndicator = (channel, voltage, color) => {
     const y = -(voltage * CANVAS_HEIGHT) / (scaleY * NUM_INTERVALS_Y);
+    const x = -(INDICATOR_WIDTH + INDICATOR_MARGIN) * (channel + 1);
+
     const roundedVoltage =
       Math.trunc(voltage * 10 ** INDICATOR_DECIMAL_PLACES) /
       10 ** INDICATOR_DECIMAL_PLACES;
 
     canvasContext.fillStyle = color;
-    // Draw triangle
     canvasContext.beginPath();
-    canvasContext.moveTo(0, y);
-    canvasContext.lineTo(-INDICATOR_WIDTH, y - INDICATOR_HEIGHT / 2);
-    canvasContext.lineTo(-INDICATOR_WIDTH, y + INDICATOR_HEIGHT / 2);
+    canvasContext.arc(x, y, INDICATOR_WIDTH / 2, 0, 2 * Math.PI);
     canvasContext.fill();
     // Draw text
-    canvasContext.font = `${INDICATOR_FONT_SIZE}px Arial`;
-    canvasContext.textAlign = "right";
+    canvasContext.font = `${INDICATOR_FONT_SIZE}px monospace`;
     canvasContext.fillText(
-      `${roundedVoltage} V`,
-      -INDICATOR_WIDTH - 4,
-      y + INDICATOR_FONT_SIZE / 2
+      `${channel}:${roundedVoltage} V`,
+      -INDICATOR_SECTION_WIDTH + 8,
+      -(CANVAS_HEIGHT / 2) + (channel + 1) * INDICATOR_FONT_SIZE
     );
+  };
+
+  const drawMinMaxLine = (channel, color) => {
+    const minY = -(min[channel] * CANVAS_HEIGHT) / (scaleY * NUM_INTERVALS_Y);
+    const maxY = -(max[channel] * CANVAS_HEIGHT) / (scaleY * NUM_INTERVALS_Y);
+    const x = -(INDICATOR_WIDTH + INDICATOR_MARGIN) * (channel + 1);
+    canvasContext.beginPath();
+    canvasContext.fillStyle = color;
+    canvasContext.strokeStyle = color;
+    canvasContext.moveTo(x - 4, minY);
+    canvasContext.lineTo(x + 4, minY);
+    canvasContext.stroke();
+    canvasContext.moveTo(x - 4, maxY);
+    canvasContext.lineTo(x + 4, maxY);
+    canvasContext.stroke();
   };
 
   onMount(() => {
