c7fdb8d0cebca22e718c49098a06d14129a0d05e
nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>
2022-11-19 07:02:44 +0100

    #45: add constants for min and max sweep
    Signed-off-by: nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>

--- Apps/frontend/src/const.js
+++ Apps/frontend/src/const.js
@@ -6,4 +6,5 @@ export const NUM_INTERVALS_X = 20;
 export const NUM_INTERVALS_Y = 10;
 
 export const NUM_CHANNELS = 10;
-export const MAX_SWEEP = 2;
+export const MIN_SWEEP  = 0.5;// <= 1
+export const MAX_SWEEP  = 2.0;// >= 1
--- Apps/frontend/src/views/SineWave.svelte
+++ Apps/frontend/src/views/SineWave.svelte
@@ -1,15 +1,14 @@
 <script>
   import { beforeUpdate, onMount } from "svelte";
   import { ColorRGBA, WebglLine, WebglPlot } from "webgl-plot";
-  import { CANVAS_HEIGHT, CANVAS_WIDTH, NUM_INTERVALS_Y, MAX_SWEEP, NUM_CHANNELS } from "../const";
+  import { CANVAS_HEIGHT, CANVAS_WIDTH, NUM_INTERVALS_Y, NUM_CHANNELS, MIN_SWEEP, MAX_SWEEP } from "../const";
   import { timeSweep } from "../stores";
 
   export let scaleY;
 
   let canvasElement;
   let webGLPlot;
-  let numberOfChannels = 10;
-  let channel_samples = Array.from(Array(numberOfChannels), () => new Array(CANVAS_WIDTH).fill(0.0))
+  let channel_samples = Array.from(Array(NUM_CHANNELS), () => new Array(CANVAS_WIDTH).fill(0.0))
   let lines = []
 
   let xArr = new Array(NUM_CHANNELS).fill(0.0);
@@ -20,13 +19,16 @@
       let xCurr = xArr[channelIndex];
       let xNew = Math.round(xCurr);
       for (let x = xLast[channelIndex] + 1; x < xNew + 1; x++) {
-        channel_samples[channelIndex][x] = samples[channelIndex]
+        channel_samples[channelIndex][x] = samples[channelIndex];
       }
       xLast[channelIndex] = xNew;
 
-      let sweep = $timeSweep[channelIndex] / 5.0;
-      xArr[channelIndex] = xCurr + sweep;
-      while (xArr[channelIndex] >= CANVAS_WIDTH) {xArr[channelIndex] -= CANVAS_WIDTH;}
+      let sweep = $timeSweep[channelIndex] / 5.0 - 1.0;
+      let fac = sweep < 0 ? MIN_SWEEP : MAX_SWEEP;
+      let delta = fac * sweep + 1.0;
+
+      xArr[channelIndex] = xCurr + delta;
+      while (xArr[channelIndex] >= CANVAS_WIDTH) { xArr[channelIndex] -= CANVAS_WIDTH; }
     }
   }
 
@@ -53,7 +55,7 @@
   }
   
   const initializeLines = () => {
-    for (let i = 0; i < numberOfChannels; i++)
+    for (let i = 0; i < NUM_CHANNELS; i++)
     {
       const color = new ColorRGBA(Math.random(), Math.random(), Math.random(), 1);
       let line = new WebglLine(color, CANVAS_WIDTH);
