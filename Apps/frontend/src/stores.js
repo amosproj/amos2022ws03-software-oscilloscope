e1f9a88d04d1cec99914650f8d348eed0b4909fa
nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>
2022-11-21 20:45:18 +0100

    #45: add review
    Signed-off-by: nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>

--- Apps/frontend/src/stores.js
+++ Apps/frontend/src/stores.js
@@ -1,6 +1,7 @@
 import { writable } from 'svelte/store';
+import { NUM_CHANNELS } from './const';
 
 
 export const osciData = writable([]);
 
-export const timeSweep = writable(new Array(10).fill(5));
+export const timeSweep = writable(new Array(NUM_CHANNELS).fill(5));
