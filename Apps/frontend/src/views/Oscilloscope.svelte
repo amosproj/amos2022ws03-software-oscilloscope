6dc7dec20ef3a46fcf8ae470822d4b225dd71a2f
Jan Degen <jan.degen@die-degens.eu>
2022-11-22 12:39:45 +0100

    Testing docker routing

--- Apps/frontend/src/views/Oscilloscope.svelte
+++ Apps/frontend/src/views/Oscilloscope.svelte
@@ -9,7 +9,7 @@
   let socket;
 
   onMount(() => {
-    socket = new WebSocket("wss://backend:9000");
+    socket = new WebSocket("wss://sosci-dev-backend-1:9000");
     socket.binaryType = "arraybuffer";
 
     socket.onopen = () => {
