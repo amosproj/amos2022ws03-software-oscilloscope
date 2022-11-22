f0188fcc22a856c10dc07c70beb3e2fdf90b33d5
Jan Degen <jan.degen@die-degens.eu>
2022-11-22 12:03:11 +0100

    Adapting web config

--- Apps/frontend/src/views/Oscilloscope.svelte
+++ Apps/frontend/src/views/Oscilloscope.svelte
@@ -9,7 +9,7 @@
   let socket;
 
   onMount(() => {
-    socket = new WebSocket(`wss://${process.env.HOST_BACKEND}`);
+    socket = new WebSocket("wss://sosci.dev.die-degens.eu/dev/api");
     socket.binaryType = "arraybuffer";
 
     socket.onopen = () => {
