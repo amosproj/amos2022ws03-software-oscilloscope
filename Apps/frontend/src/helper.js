8264a6d8aa4c06a78d311ea18858672bd8124a72
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-11-23 16:19:16 +0100

    #71: lint and format legacy

--- Apps/frontend/src/helper.js
+++ Apps/frontend/src/helper.js
@@ -1,15 +1,14 @@
 import { INDICATOR_DECIMAL_PLACES } from "./const";
 
-
 /**
  * Returns hex string for array of rgb values.
  *
  * @param {[number, number, number]} rgb Array of rgb values
  * @returns {string} rgba string
  */
- export const rgbArrayToRGBAString = (rgb) =>
- `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 1)`;
+export const rgbArrayToRGBAString = (rgb) =>
+  `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 1)`;
 
 export const roundVoltage = (voltage) =>
- Math.trunc(voltage * 10 ** INDICATOR_DECIMAL_PLACES) /
- 10 ** INDICATOR_DECIMAL_PLACES;
+  Math.trunc(voltage * 10 ** INDICATOR_DECIMAL_PLACES) /
+  10 ** INDICATOR_DECIMAL_PLACES;
--- Apps/frontend/src/views/TimeSweepSlider.svelte
+++ Apps/frontend/src/views/TimeSweepSlider.svelte
@@ -1,8 +1,14 @@
 <script>
-    import { timeSweep } from "../stores";
-    export let channel;
+  import { timeSweep } from "../stores";
+  export let channel;
 </script>
 
 <div>
-    <input type="range" min="0" max="10" class="slider" bind:value={$timeSweep[channel]}>
+  <input
+    type="range"
+    min="0"
+    max="10"
+    class="slider"
+    bind:value={$timeSweep[channel]}
+  />
 </div>
