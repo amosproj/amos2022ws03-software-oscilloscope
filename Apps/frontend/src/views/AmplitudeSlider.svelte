822177cfdb0ca8a2cb3005c71876c1b4cbc29292
Jens Wächtler <jens.f.waechtler@fau.de>
2022-11-23 09:56:23 +0100

    Add slider for amplitude adjustment
    Signed-off-by: Jens Wächtler <jens.f.waechtler@fau.de>

---
+++ Apps/frontend/src/views/AmplitudeSlider.svelte
@@ -0,0 +1,9 @@
+<script>
+    import {amplitudeAdjustment} from "../stores";
+
+    export let channel;
+</script>
+
+<div>
+  <input type="range" min="0" max="10" class="slider" bind:value={$amplitudeAdjustment[channel]}>
+</div>
