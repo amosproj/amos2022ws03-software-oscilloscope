a8fe5602f1d012716ea9f13be084ad4ed4baf15e
Jens Wächtler <jens.f.waechtler@fau.de>
2022-11-25 09:39:45 +0100

    Accept linter changes
    Signed-off-by: Jens Wächtler <jens.f.waechtler@fau.de>

--- Apps/frontend/src/views/AmplitudeSlider.svelte
+++ Apps/frontend/src/views/AmplitudeSlider.svelte
@@ -1,9 +1,15 @@
 <script>
-    import {amplitudeAdjustment} from "../stores";
+  import { amplitudeAdjustment } from "../stores";
 
-    export let channel;
+  export let channel;
 </script>
 
 <div>
-  <input type="range" min="0" max="10" class="slider" bind:value={$amplitudeAdjustment[channel]}>
+  <input
+    type="range"
+    min="0"
+    max="10"
+    class="slider"
+    bind:value={$amplitudeAdjustment[channel]}
+  />
 </div>
