7c800317f5e1fc463ef28f4a49534b6b8979522c
Jens Wächtler <jens.f.waechtler@fau.de>
2022-11-28 14:50:14 +0100

    Add slider component for amplitude adjustment
    Signed-off-by: Jens Wächtler <jens.f.waechtler@fau.de>

--- Apps/frontend/src/views/AmplitudeSlider.svelte
+++ Apps/frontend/src/views/AmplitudeSlider.svelte
@@ -1,15 +1,25 @@
 <script>
-  import { amplitudeAdjustment } from "../stores";
+  import { amplitudeAdjustment } from "../stores.js";
+  import { MIN_AMPLITUDE, MAX_AMPLITUDE } from "../const.js";
+  export let onInput = (amplitude) => {
+    return amplitude;
+  };
 
   export let channel;
+  let amplitude = 1.0;
 </script>
 
 <div>
   <input
     type="range"
-    min="0"
-    max="10"
+    bind:value={amplitude}
+    on:input={() => {
+      $amplitudeAdjustment[channel] = amplitude;
+      onInput(amplitude);
+    }}
+    min={MIN_AMPLITUDE}
+    max={MAX_AMPLITUDE}
+    step="0.01"
     class="slider"
-    bind:value={$amplitudeAdjustment[channel]}
   />
 </div>
