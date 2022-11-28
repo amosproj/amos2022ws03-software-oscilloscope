1a3f64b6a70b0e3ebea74dd43c6231be42efced4
Jens Wächtler <jens.f.waechtler@fau.de>
2022-11-28 14:46:05 +0100

    Add store for amplitudeAdjustment
    Signed-off-by: Jens Wächtler <jens.f.waechtler@fau.de>

--- Apps/frontend/src/stores.js
+++ Apps/frontend/src/stores.js
@@ -5,4 +5,4 @@ export const osciData = writable([]);
 
 export const timeSweep = writable(new Array(NUM_CHANNELS).fill(5));
 
-export const amplitudeAdjustment = writable(new Array(NUM_CHANNELS).fill(5));
+export const amplitudeAdjustment = writable(new Array(NUM_CHANNELS).fill(1));
