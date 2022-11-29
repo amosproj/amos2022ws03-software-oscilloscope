1f6023b15e1e06a6daf11461a378781788a229e9
Jens Wächtler <jens.f.waechtler@fau.de>
2022-11-29 19:29:11 +0100

    Use loop for testing
    Signed-off-by: Jens Wächtler <jens.f.waechtler@fau.de>

--- Apps/frontend/cypress/e2e/3-slider/amplitude.cy.js
+++ Apps/frontend/cypress/e2e/3-slider/amplitude.cy.js
@@ -1,19 +1,12 @@
-describe("amplitudeSlider", () => {
+describe.only("amplitudeSlider", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173/");
   });
 
   it("is visible", () => {
-    cy.get('[data-cy="amplitudeSlider-0"]');
-    cy.get('[data-cy="amplitudeSlider-1"]');
-    cy.get('[data-cy="amplitudeSlider-2"]');
-    cy.get('[data-cy="amplitudeSlider-3"]');
-    cy.get('[data-cy="amplitudeSlider-4"]');
-    cy.get('[data-cy="amplitudeSlider-5"]');
-    cy.get('[data-cy="amplitudeSlider-6"]');
-    cy.get('[data-cy="amplitudeSlider-7"]');
-    cy.get('[data-cy="amplitudeSlider-8"]');
-    cy.get('[data-cy="amplitudeSlider-9"]');
+    Cypress._.times(10, (i) => {
+      cy.get('[data-cy="amplitudeSlider-' + i + '"]').should("be.visible");
+    });
   });
 
   it("change value", () => {
