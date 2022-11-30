a28ff3a21821f09da8950f669dd94c3ed71d8a1a
nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>
2022-11-30 06:47:13 +0100

    fix test file #26
    Signed-off-by: nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>

--- Apps/frontend/cypress/e2e/2-control-panel/reset-button.cy.js
+++ Apps/frontend/cypress/e2e/2-control-panel/reset-button.cy.js
@@ -5,11 +5,6 @@ describe("everything visible on front page", () => {
     cy.visit("http://localhost:5173/");
   });
 
-  it("displays btn", () => {
-    cy.get(".control-panel").should("be.visible");
-    cy.get(".button-reset").should("be.visible");
-  });
-
   it("click btn", () => {
     // We use the `cy.get()` to get the canvas element
     cy.get("#button-reset").click();
