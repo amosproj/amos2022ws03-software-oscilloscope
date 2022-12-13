63fbbbb7ba0b1a3204c4fb815152f798e0128a41
Jan Degen <jan.degen@die-degens.eu>
2022-12-13 19:57:14 +0100

    Fixed missing common slider functionality from merge

--- Apps/frontend/src/components/TimeSweepSlider.svelte
+++ Apps/frontend/src/components/TimeSweepSlider.svelte
@@ -14,5 +14,5 @@
 </script>
 
 <div class="control-panel-items">
-  <input type="range" min="0" max="10" bind:value={$timeSweep[channel]} />
+  <input type="range" min="0" max="10" bind:value={$timeSweep[channel]} on:change={handleChange} />
 </div>
