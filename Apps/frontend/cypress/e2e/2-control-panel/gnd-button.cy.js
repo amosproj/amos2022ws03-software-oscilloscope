6f65565268176b1c1a2f38a0c779a794804bb12f
nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>
2022-12-14 19:55:54 +0100

    add test for #127
    Signed-off-by: nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>

---
+++ Apps/frontend/cypress/e2e/2-control-panel/gnd-button.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("everything visible on front page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
+
+  it("click btn", () => {
+    // We use the `cy.get()` to get the canvas element
+    cy.get("#btn-gnd").click();
+  });
+});
