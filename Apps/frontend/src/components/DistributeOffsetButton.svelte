dedd22a1693111c4574c668fa0754d8cc0947737
Jens Wächtler <jens.f.waechtler@fau.de>
2022-12-20 17:31:54 +0100

    Add a button for dispatching distributeOffset events
    Co-authored-by: Ingrid Münch <canada.journey@googlemail.com>
    Signed-off-by: Jens Wächtler <jens.f.waechtler@fau.de>

---
+++ Apps/frontend/src/components/DistributeOffsetButton.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import { NUM_CHANNELS } from "../const.js";
+
+  const dispatch = createEventDispatcher();
+  const handleClick = async () => {
+    dispatch("distributeOffset", { offset: calculateOffset() });
+  };
+  function calculateOffset() {
+    // offset is calculated based on the number of channels between -1 and 1
+    return 2 / (NUM_CHANNELS + 1);
+  }
+</script>
+
+<button id="distribute-button" on:click={handleClick}>Distribute waves</button>
+
+<style>
+  #distribute-button {
+    border-style: solid;
+    border-color: grey;
+  }
+</style>
