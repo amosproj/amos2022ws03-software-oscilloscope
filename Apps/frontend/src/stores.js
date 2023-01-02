ea2d46dadef00a7b3aaf4ddbaa21f8d1d8bf69e6
Marcel Schöckel <marcel.schoeckel@gmail.com>
2023-01-02 19:38:58 +0100

    Add documentation to value stores

--- Apps/frontend/src/stores.js
+++ Apps/frontend/src/stores.js
@@ -3,11 +3,28 @@ import { NUM_CHANNELS } from "./const";
 
 export const osciData = writable([]);
 
+/**
+ * @type {import("svelte/store").Writable<number[]>}
+ * Stores the timeSweep factor of each channel
+ */
 export const timeSweep = writable(new Array(NUM_CHANNELS + 1).fill(5));
 
-// store for the amplification factors of each channel
+/**
+ * @type {import("svelte/store").Writable<number[]>}
+ * Stores the amplification factor of each channel
+ */
 export const amplitudeAdjustment = writable(new Array(NUM_CHANNELS).fill(1));
 
+/**
+ * @type {import("svelte/store").Writable<boolean[]>}
+ * Stores the thickness state of each channels
+ */
 export const thicknessAdjustment = writable(
   new Array(NUM_CHANNELS).fill(false)
 );
+
+/**
+ * @type {import("svelte/store").Writable<boolean[]>}
+ * Stores the activation state of each channels
+ */
+export const channelEnabled = writable(new Array(NUM_CHANNELS).fill(false));
