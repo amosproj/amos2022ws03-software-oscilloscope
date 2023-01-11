dbcc60b554c250da2e4fcad3254d0a8ba09c89d3
nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>
2023-01-11 10:38:21 +0100

    #127 test with new ui
    Signed-off-by: nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>

---
+++ Apps/frontend/cypress/e2e/3-control-panel-top/gnd-button.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe("resetButton", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
+
+  it("is visible", () => {
+    cy.get(".control-panel--top_general").should("be.visible");
+    cy.get('[data-cy="gnd-button"]').should("be.visible");
+  });
+
+  it("is clickable", () => {
+    cy.get('[data-cy="gnd-button"]').click();
+  });
+});
