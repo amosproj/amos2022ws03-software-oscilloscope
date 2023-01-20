71abde8d209633bdffb6db65eb427a62c2116ce3
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2023-01-20 18:13:25 +0100

    use store for isActive
    Signed-off-by: Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>

--- Apps/frontend/src/views/TextIndicators.svelte
+++ Apps/frontend/src/views/TextIndicators.svelte
@@ -2,12 +2,16 @@
 
 import { roundVoltage } from "../helper.js";
 import { LINE_COLORS_RGBA, NUM_CHANNELS } from "../const.js";
+import { channelActivated } from "../stores.js";
 
 
 let min = Array(NUM_CHANNELS).fill(0.0);
 let max = Array(NUM_CHANNELS).fill(0.0);
-let startStopLine = Array(NUM_CHANNELS).fill(true);
+let isActive = Array(NUM_CHANNELS).fill(true);
 
+channelActivated.subscribe((newIsActive) => {
+  isActive = newIsActive;
+});
 
 /**
  * Trigger a rerender of the indicators with given sample array.
@@ -16,7 +20,7 @@ let startStopLine = Array(NUM_CHANNELS).fill(true);
  */
 export const update = (samples) => {
   for (let channel = 0; channel < NUM_CHANNELS; channel++) {
-    if (!startStopLine[channel]) continue;
+    if (!isActive[channel]) continue;
     if (samples[channel] > max[channel]) {
       max[channel] = samples[channel];
     }
@@ -29,16 +33,6 @@ export const update = (samples) => {
 export const reset = () => {
   min = Array(NUM_CHANNELS).fill(0.0);
   max = Array(NUM_CHANNELS).fill(0.0);
-};
-
-/**
- * Start or stop indicator updates of a channel.
- *
- * @param {number} channel
- * @param {boolean} hasStarted
- */
-export const startStopChannelI = (channel, hasStarted) => {
-  startStopLine[channel] = hasStarted;
 };
 
 </script>
