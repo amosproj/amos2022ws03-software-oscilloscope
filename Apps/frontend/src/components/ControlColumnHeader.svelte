e881e372a32721cbfcf0bc957e8323054fdc0f55
nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>
2023-01-25 17:26:55 +0100

    #198: tooltip to Header component
    Signed-off-by: nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>

--- Apps/frontend/src/components/ControlColumnHeader.svelte
+++ Apps/frontend/src/components/ControlColumnHeader.svelte
@@ -1,10 +1,18 @@
 <script>
   export let label;
   export let icon;
+  export let tooltip = null;
+  import { Tooltip } from "sveltestrap";
+  let div;
 </script>
 
-<div class="icon icon--{icon}" />
-<div>{label}</div>
+<div bind:this={div} style="cursor: help;">
+  <div class="icon icon--{icon}" />
+  <div>{label}</div>
+</div>
+<Tooltip target={div} placement="bottom">
+  {tooltip != null ? tooltip : label}
+</Tooltip>
 
 <style>
   .icon {
