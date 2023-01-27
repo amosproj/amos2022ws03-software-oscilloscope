2dabb0e4168d44b3f0f7113bb3f1d910e8ad7d08
Jens Wächtler <jens.f.waechtler@fau.de>
2023-01-27 12:16:16 +0100

    Add tests for extracted functions
    Signed-off-by: Jens Wächtler <jens.f.waechtler@fau.de>

--- Apps/generator/src/main.rs
+++ Apps/generator/src/main.rs
@@ -53,3 +53,30 @@ fn setup_loop(config: &Config) -> (TenChannelSampleGenerator, Interval) {
     let package_interval = time::interval(package_interval);
     (generator, package_interval)
 }
+
+// test
+#[cfg(test)]
+mod test {
+    use std::net::IpAddr;
+    use super::*;
+
+    #[tokio::test]
+     async fn test_run() {
+        let (mut generator, mut interval) = setup_loop(&Config::build_from_environment_variables());
+        assert_eq!(generator.next().len(), 10);
+        assert_ne!(interval.tick().await, time::Instant::now());
+    }
+
+    #[test]
+    fn test_setup() {
+        let (config, socket) = setup();
+        let config_test = Config::build_from_environment_variables();
+        assert_eq!(config_test.packages_per_second, config.packages_per_second);
+        assert_eq!(config_test.signal_frequency, config.signal_frequency);
+        assert_eq!(config_test.signal_amplitude, config.signal_amplitude);
+        assert_eq!(config_test.host, config.host);
+        assert_eq!(config_test.target, config.target);
+        assert_eq!(socket.local_addr().unwrap().ip(), IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)));
+    }
+
+}
