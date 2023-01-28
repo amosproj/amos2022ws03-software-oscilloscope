b2530e72c1a6a129d3f20f6723556c1068f2ae64
nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>
2023-01-28 22:06:06 +0100

    requested change #198: styling to scss
    Signed-off-by: nicolaskolbenschlag <nicolas.kolbenschlag@googlemail.com>

--- Apps/frontend/src/components/ControlColumnHeader.svelte
+++ Apps/frontend/src/components/ControlColumnHeader.svelte
@@ -6,19 +6,10 @@
   let div;
 </script>
 
-<div bind:this={div} style="cursor: help;">
+<div bind:this={div} class="control-column-header">
   <div class="icon icon--{icon}" />
   <div>{label}</div>
 </div>
 <Tooltip target={div} placement="bottom">
   {tooltip != null ? tooltip : label}
 </Tooltip>
-
-<style>
-  .icon {
-    width: 100%;
-    height: 24px;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-</style>
