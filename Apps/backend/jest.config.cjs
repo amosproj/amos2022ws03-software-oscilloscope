e5e00bb02bfe18a35cd7090c6590d81ad42121f2
Jan Degen <jan.degen@fau.de>
2023-02-03 16:07:16 +0100

    Added coverage threshold

--- Apps/backend/jest.config.cjs
+++ Apps/backend/jest.config.cjs
@@ -1,5 +1,11 @@
 module.exports = {
     testEnvironment: 'jest-environment-node',
     testMatch: ['**/src/tests/**/*.spec.js'],
-    collectCoverageFrom: ['**/src/**/*.js', '!**/node_modules/**', '!**/vendor/**']
+    collectCoverageFrom: ['**/src/**/*.js', '!**/node_modules/**', '!**/vendor/**'],
+    "coverageThreshold": {
+      "global": {
+        "lines": 70,
+        "statements": 70
+      }
+    }
   };
